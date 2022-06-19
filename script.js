8864ed81c066308e464fbe443d572857dd377809
Nikolay Vinogradov <was.bic@gmail.com>
2022-06-20 01:43:55 +0300

    Исправил

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 // 'use strict'
 
-let lang = ('ru');
-
-if (lang == 'ru') {
-  let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-  console.log(arr[3]);
-} 
-if (lang == 'en') {
-  let arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-  console.log(arr[3]);
-}
-switch (lang) {
-	case 'ru':
-		let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-	break;
-	case 'en':
-		arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-	break;
-}
+let lang = 'ru';
 let arr = {
 	'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
 	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
 };
+if (lang == 'ru') {
+  arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  console.log(arr[3]);
+} 
+if (lang == 'en') {
+  arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+  console.log(arr[3]);
+}
+switch (lang) {
+	case 'ru':
+		arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+	break;
+	case 'en':
+		arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+		console.log(arr[4]);
+	break;
+}
+
 alert(arr[lang]);
 
 let namePerson = 'Сергей';
